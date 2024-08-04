2dc9f2e6494ba53ced93bf8f46fecab9aaf59df2
zu-silverhair <zuzaraza@mail.ru>
2024-08-04 09:09:33 +0500

    Шаг 2
    Расходы на рекламу по модели атрибуции Last Paid Click

---
+++ aggregate_last_paid_click.sql
@@ -0,0 +1,40 @@
+---Напишите запрос для атрибуции лидов по модели Last Paid Click топ-10 записей
+with tab as(
+	select 
+		s.visitor_id,
+		s.visit_date,
+		s."source" as utm_source,
+		s.medium as utm_medium,
+		s.campaign as utm_campaign,
+		l.lead_id,
+		l.created_at,
+		l.amount,
+		l.closing_reason,
+		l.status_id,
+		row_number() over(partition by s.visitor_id order by s.visit_date) as rang
+	from sessions s 
+	left join leads l on s.visitor_id  = l.visitor_id 
+		and s.visit_date < l.created_at
+	where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+	order by s.visit_date desc
+)
+
+select 
+		visitor_id,
+		visit_date,
+		utm_source,
+		utm_medium,
+		utm_campaign,
+		lead_id,
+		created_at,
+		amount,
+		closing_reason,
+		status_id
+from tab
+where rang = 1
+order by amount desc NULLS last, visit_date asc, utm_source, utm_medium, utm_campaign
+limit 10;
+
+
+
+	
