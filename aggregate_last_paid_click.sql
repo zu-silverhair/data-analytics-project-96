9b268661ce3fd2e0f4e0c63ceebd69ef6e53dca7
zu-silverhair <zuzaraza@mail.ru>
2024-09-04 02:50:43 +0500

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -11,13 +11,15 @@ with lpc as (
         l.amount,
         l.closing_reason,
         l.status_id,
-        row_number() over (partition by s.visitor_id order by s.visit_date desc) as rang
-    from sessions s
-    left join leads l on s.visitor_id  = l.visitor_id 
+        row_number() over (partition by s.visitor_id
+        order by s.visit_date desc) as rang
+    from sessions as s
+    left join leads l on s.visitor_id  = l.visitor_id
         and s.visit_date <= l.created_at
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-)
-, unoin_ads as (
+),
+
+unoin_ads as (
     select
         vk.campaign_date::date,
         vk.utm_source,
@@ -34,7 +36,7 @@ with lpc as (
         ya.utm_campaign,
         sum(ya.daily_spent) as daily_spent
     from ya_ads as ya
-    group by 1,2,3,4
+    group by 1, 2, 3, 4
 )
 
 select
@@ -43,23 +45,25 @@ select
     lpc.utm_source,
     lpc.utm_medium,
     lpc.utm_campaign,
-     u.daily_spent as total_cost,
+    u.daily_spent as total_cost,
     count(distinct lpc.lead_id) as leads_count,
-    count(lpc.lead_id)  filter (
-         where lpc.status_id = 142) as purchases_count,
+    count(lpc.lead_id) filter (
+        where lpc.status_id = 142
+    ) as purchases_count,
     sum(lpc.amount) as revenue
 from lpc
 left join unoin_ads as u
-        on u.campaign_date = lpc.visit_date
+    on u.campaign_date = lpc.visit_date
         and u.utm_source = lpc.utm_source
         and u.utm_medium = lpc.utm_medium
         and u.utm_campaign = lpc.utm_campaign
 where lpc.rang = 1
 group by 1, 3, 4, 5, 6
-order by revenue desc nulls last,
-    lpc.visit_date asc, 
+order by
+    lpc.visit_date asc,
     visitors_count desc,
-    lpc.utm_source asc, 
-    lpc.utm_medium asc, 
-    lpc.utm_campaign asc
+    lpc.utm_source asc,
+    lpc.utm_medium asc,
+    lpc.utm_campaign asc,
+    revenue desc nulls last
 limit 15;
