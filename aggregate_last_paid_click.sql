54909b9683d67c939f0e18ed7edae6ffa3b436f3
zu-silverhair <zuzaraza@mail.ru>
2024-09-04 09:27:19 +0500

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -40,27 +40,42 @@ unoin_ads as (
         sum(ya.daily_spent) as daily_spent
     from ya_ads as ya
     group by 1, 2, 3, 4
-)
-select
-    lpc.visit_date,
-    count(lpc.visitor_id) as visitors_count,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
-    u.daily_spent as total_cost,
-    count(distinct lpc.lead_id) as leads_count,
-    count(lpc.lead_id) filter (
-        where lpc.status_id = 142
+),
+
+metrics as (
+    select
+        lpc.visit_date,
+        count(lpc.visitor_id) as visitors_count,
+        lpc.utm_source,
+        lpc.utm_medium,
+        lpc.utm_campaign,
+        u.daily_spent as total_cost,
+        count(distinct lpc.lead_id) as leads_count,
+        count(lpc.lead_id) filter (
+            where lpc.status_id = 142
         ) as purchases_count,
-    sum(lpc.amount) as revenue
-from lpc
-left join unoin_ads as u
-    on
-        u.campaign_date = lpc.visit_date
-        and u.utm_source = lpc.utm_source
-        and u.utm_medium = lpc.utm_medium
-        and u.utm_campaign = lpc.utm_campaign
-where lpc.rang = 1
-group by 1, 3, 4, 5, 6
-order by revenue desc nulls last, visitors_count desc
+        sum(lpc.amount) as revenue
+    from lpc
+    left join unoin_ads as u
+        on
+            u.campaign_date = lpc.visit_date
+            and u.utm_source = lpc.utm_source
+            and u.utm_medium = lpc.utm_medium
+            and u.utm_campaign = lpc.utm_campaign
+    where lpc.rang = 1
+    group by 1, 3, 4, 5, 6
+)
+
+select
+        visit_date,
+        visitors_count,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        total_cost,
+        leads_count,
+        purchases_count,
+        revenue
+from metrics
+    order by revenue desc nulls last, visitors_count desc
 limit 15;
