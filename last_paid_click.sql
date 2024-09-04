b8d775750fb532d26e28248e0125b5756df0ac1d
zu-silverhair <zuzaraza@mail.ru>
2024-09-04 09:06:46 +0500

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -16,9 +16,10 @@ with tab as (
             partition by s.visitor_id order by s.visit_date desc
         ) as rang
     from sessions as s
-    left join leads as l on 
-        s.visitor_id = l.visitor_id
-        and s.visit_date < l.created_at
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date < l.created_at
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
     order by s.visit_date desc
 )
@@ -36,15 +37,6 @@ select
     status_id
 from tab
 where rang = 1
-order by 
-    visitor_id,
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    lead_id,
-    created_at,
-    amount desc nulls last,
-    closing_reason,
-    status_id
+order by
+    amount desc nulls last
 limit 10;
